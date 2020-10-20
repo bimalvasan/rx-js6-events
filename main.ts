31db38be0a4cbbca14fc9a1596090b9b931874d2
bimalvasan <bimalvasan@gmail.com>
2020-10-20 15:56:31 -0500

    Create observerable

--- main.ts
+++ main.ts
@@ -3,7 +3,12 @@ import { Observable } from "rxjs";
 
 let numbers = [1, 3, 7, 10];
 
-let source = from(numbers);
+let source = Observable.create(observer => {
+    for (let n of numbers) {
+        observer.next(n);
+    }
+    observer.complete();
+}); //from(numbers);
 
 source.subscribe(
     value => console.log(`Value: ${value}`), //Next
